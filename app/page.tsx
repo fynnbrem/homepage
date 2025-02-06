7998a2fbc8b213c6c24a19db4da6dd2123d430bb
Fynn Bremser <fynn.bremser@gmx.de>
2025-02-06 18:47:25 +0100

    Added simple gravitational force functions.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
 "use client"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useEventListener} from "usehooks-ts";
+import {Vector2} from "math.gl";
 
-type Vector = {
-    x: number, y: number
-};
+
+type Ball = { // TODO: Update DocStrings to use the proper terminology.
+    pos: Vector2,
+    vel: Vector2 | null,
+    mass: number,
+    radius: number | null,
+}
+
+
+const gravity = 10
+
+const tps = 60
+const interval = Math.round(1000 / 60)
+
+const mainBall: Ball = {
+    pos: new Vector2(0, 0),
+    vel: new Vector2(0, 0),
+    mass: 20,
+    radius: 25
+}
+
+const mouseBall: Ball = {
+    pos: new Vector2(0, 0),
+    vel: new Vector2(0, 0),
+    mass: 10,
+    radius: null
+}
+
 
 export default function Home() {
     const [mousePos, setMousePos] = useState([0, 0])
 
+    useEffect(() => {
+        const intervalTimer = setInterval(() => {
+            moveBalls([mainBall, mouseBall]);
+        }, interval);
+
+        return () => clearInterval(intervalTimer);
+    }, []);
+
+    const mouseMass = 10
     const radius = 25
+
 
     useEventListener("mousemove", (e: MouseEvent) => {
         setMousePos([e.clientX, e.clientY]);
@@ -32,12 +68,57 @@ export default function Home() {
     );
 }
 
-function getForce(pos1: Vector, pos2: Vector, power: number): Vector {
-    const distance = Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2);
-    const forceScale = power / (distance ** 3)
-    // Square scaling by the nature and an extra division by the distance to normalize the vector delta.
-    return {
-        x: (pos2.x - pos1.x) * forceScale,
-        y: (pos2.y - pos1.y) * forceScale,
+function moveBalls(balls: Ball[]): void {
+    balls.forEach(
+        applyGravity
+    )
+    for (let i = 0; i++; i < balls.length) {
+        for (let j = 0; j++; j < i) { // Limit the iterations to `i - 1` as to only include unique and unequal pairs.
+            applyForce(balls[i], balls[j])
+        }
     }
+
+    balls.forEach(applyVelocity)
+
+}
+
+
+/** Modifies the position of the MassPoint in accordance with its current velocity.*/
+function applyVelocity(ball: Ball): void {
+    if (ball.vel) {
+        ball.pos.add(ball.vel)
+    }
+
+}
+
+/** Modifies the velocity of both MassPoints in accordance to the force generated between them.*/
+function applyForce(ball1: Ball, ball2: Ball): void {
+    const force = getForce(ball1, ball2)
+    if (ball1.vel) {
+        const acc1 = force.scale(1 / ball1.mass)
+        ball1.vel.add(acc1)
+    }
+    if (ball2.vel) {
+        const acc2 = force.scale(1 / ball2.mass).negate()
+        ball2.vel.add(acc2)
+    }
+
+}
+
+/**Modifies the velocity of the MassPoint in accordance with the global gravity.*/
+function applyGravity(ball: Ball) {
+    if (ball.vel) {
+        ball.vel.y = ball.vel.y + (gravity / ball.mass)
+    }
+
+}
+
+
+/** Return the gravitational force between the two MassPoints, determined by their distance and mass.
+ *  The vector points from `ball1` to `ball2`.*/
+function getForce(ball1: Ball, ball2: Ball): Vector2 {
+    const distance = ball1.pos.distance(ball2.pos);
+    const forceScale = (ball1.mass * ball2.mass) / (distance ** 3)
+    // Square scaling by the nature and an extra division by the distance to normalize the vector delta.
+    return ball2.pos.clone().subtract(ball1.pos).scale(forceScale)
 }
